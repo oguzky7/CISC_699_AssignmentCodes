8f31898084ead651a648bd7cb0256cfb4f0ac9f0
oguzky7 <oguzky7@gmail.com>
2024-09-23 18:17:27 -0500

    delete everytinhg

--- main.py
+++
@@ -1,7 +0,0 @@
-from utils.MyBot import start_bot
-from utils.Config import Config
-
-# Initialize and run the bot
-if __name__ == "__main__":
-    print("Bot is starting...")
-    start_bot(Config.DISCORD_TOKEN)  # Start the bot using the token from config
